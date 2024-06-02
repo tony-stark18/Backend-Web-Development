0f2e0fc15c2774351841fd52da88eb47f010cf88
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-02 14:34:10 +0530

    Routing using HTTP module

--- 1-node-farm/httpModule.js
+++ 1-node-farm/httpModule.js
@@ -1,8 +1,21 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-    // console.log(req);
-    res.end('Hello From the Server!');
-})
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the Product");
+  } else if (pathName === "/") {
+    res.end("Hello From the Server!");
+  } else {
+    res.writeHead(404,{
+        'Content-type':'text/html',
+        'my-own-header':'hello World'
+    });
+    res.end("<h1>Page not found!<h1>"); // Send the "Page not found" response
+  }
+});
 
 server.listen(8000,'127.0.0.1',()=>{
     console.log('Listening to requests on port 8000')
