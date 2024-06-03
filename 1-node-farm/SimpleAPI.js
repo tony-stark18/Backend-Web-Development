fd09311f3301396c5e862f8cebd6e5652acfd295
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-03 16:45:36 +0530

    Creating my own module..

--- 1-node-farm/SimpleAPI.js
+++ 1-node-farm/SimpleAPI.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
@@ -8,20 +9,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp,product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%FROM%}/g,product.from);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%ID%}/g,product.id);
-    output = output.replace(/{%DESCRIPTION%}/g,product.description);
-    if(!product.organic){
-        output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
-    }
-    return output;
-}
 
 const server = http.createServer((req, res) => {
 
