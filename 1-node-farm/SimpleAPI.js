0bfd3c3d76e1935883e5f10acf7d6b44adc1ffee
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-09 16:29:53 +0530

    ...

--- 1-node-farm/SimpleAPI.js
+++ 1-node-farm/SimpleAPI.js
@@ -1,32 +1,41 @@
 const http = require("http");
 const fs = require("fs");
-const url = require('url');
-const replaceTemplate = require('./modules/replaceTemplate');
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-
 const server = http.createServer((req, res) => {
-
   const { query, pathname } = url.parse(req.url, true);
-  
-  if (pathname === "/overview" || pathname==="/") {
+
+  if (pathname === "/overview" || pathname === "/") {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el)).join('');
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
   } else if (pathname === "/product") {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const product = dataObj[query.id]
-    const output = replaceTemplate(tempProduct,product);
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
     res.end(output);
   } else if (pathname === "/api") {
     res.writeHead(200, {
