eac35462ef76b2b719120cd286cba65999261820
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-18 22:43:51 +0530

    Creating a TestTour to make sure everything is working just fine

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -17,21 +17,36 @@ mongoose
   });
 
 const tourSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,'A Tour must have a Name'],
-    unique:true
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a Name'],
+    unique: true,
   },
-  rating:{
-    type:Number,
-    default:4.5
+  rating: {
+    type: Number,
+    default: 4.5,
   },
-  price:{
-    type:Number,
-    required:[true,'A Tour must have a price']
-  }
-})
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a price'],
+  },
+});
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hikerr',
+  rating: '4.7',
+  price: 497,
+}); 
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
