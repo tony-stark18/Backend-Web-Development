b2acc95a1a0ae55ec355f677f84ca2e97e8e2139
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-18 22:26:31 +0530

    Setting up the mongoDB database and using Mongoose with Node.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,24 @@
 const dotenv = require('dotenv');
+const app = require('./app');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
+  });
 
-const port = process.env.port || 3000;
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
