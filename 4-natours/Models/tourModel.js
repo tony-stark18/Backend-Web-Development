6c66a610e47cafa9efe0ea54658c77a4dda5f9a3
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-28 23:52:53 +0530

    Created Tour Model

--- 4-natours/Models/tourModel.js
+++ 4-natours/Models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A Tour must have a Name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour must have a groupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A Tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A Tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A Tour must have a Summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
-const Tour = new mongoose.model('Tour',tourSchema);
+const Tour = new mongoose.model('Tour', tourSchema);
 module.exports = Tour;
