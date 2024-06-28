a07e5381d443dda9f1f760c41e4c8cd54b7ff246
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-06-28 21:41:41 +0530

    Yes, I am Back :)

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -16,38 +16,6 @@ mongoose
     console.error('DB connection error:', err);
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A Tour must have a Name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A Tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hikerr',
-  rating: '4.7',
-  price: 497,
-}); 
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
