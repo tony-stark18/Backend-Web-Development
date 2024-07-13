2c319190519eb3a91b141c309cb098ed59487b03
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-07-14 00:17:02 +0530

    Some more aggregation stages..

--- 4-natours/Routes/tourRoutes.js
+++ 4-natours/Routes/tourRoutes.js
@@ -7,11 +7,13 @@ const {
   updateTour,
   deleteTour,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
